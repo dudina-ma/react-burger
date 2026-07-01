d84a088bfc7d506bf0ed48de220fdebf6962e103
dudina-ma <dudina27maria@gmail.com>
2026-07-01 11:49:56 +0200

    chore: add default middleware for async actions

--- src/services/store.ts
+++ src/services/store.ts
@@ -4,6 +4,7 @@ import { rootReducer } from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export type TRootState = ReturnType<typeof rootReducer>;
